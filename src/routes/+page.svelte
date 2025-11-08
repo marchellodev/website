e093d6acccd5b28b6fe651642b38d118799363d8
marchellodev <marchellodev@gmail.com>
2025-11-08 22:53:13 +0100

    init shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script>
+  import { Button } from '$lib/shadcn/components/ui/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<Button variant="link" href="https://google.com">Click Me</Button>
+
 <p>
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a>
-   to read the documentation
+  to read the documentation
 </p>
