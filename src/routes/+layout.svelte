fb3f3e3f1eb0526326df6b54dc3562669749fead
marchellodev <marchellodev@gmail.com>
2025-11-09 20:53:33 +0100

    basic layout & first page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import '../app.css';
   import favicon from '$lib/assets/favicon.svg';
+  import { Button } from '$lib/shadcn/components/ui/button';
+  import { cn } from '$lib/shadcn/utils';
+  import { page } from '$app/state';
 
   let { children } = $props();
 </script>
@@ -9,4 +12,28 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+{#snippet navButton(opts: { text: string; url: string; newTab?: boolean })}
+  {@const isActive = page.url.pathname == opts.url}
+
+  <Button
+    variant="link"
+    size="lg"
+    href={opts.url}
+    class={cn('px-4 text-lg', isActive ? 'font-medium' : 'text-muted-foreground/90')}
+    target={opts.newTab ? '_blank' : '_self'}>
+    <p>
+      <span class="text-muted-foreground/50">#</span>
+      {opts.text}
+    </p>
+  </Button>
+{/snippet}
+
+<div class="mx-auto max-w-5xl p-2 text-lg">
+  <header class="bg-muted px-6 py-4">
+    {@render navButton({ text: 'About me', url: '/' })}
+    {@render navButton({ text: 'CV', url: 'https://mark.vin/cv.pdf', newTab: true })}
+  </header>
+  <main class="bg-muted/30 px-6 py-6">
+    {@render children()}
+  </main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,108 @@
-<script>
+<script lang="ts">
   import { Button } from '$lib/shadcn/components/ui/button';
+  import { ArrowUpRight, Copy, ExternalLink } from '@lucide/svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<Button variant="link" href="https://google.com">Click Me</Button>
+{#snippet heading({ text }: { text: string })}
+  <p class="mt-8 mb-2">
+    <span class="text-muted-foreground/50">###</span>
+    {text}
+  </p>
+{/snippet}
 
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a>
-  to read the documentation
-</p>
+{#snippet separator()}
+  <p class="text-muted-foreground/50 mt-1 mb-4">---</p>
+{/snippet}
+
+{#snippet line({ text }: { text: string })}
+  <p>
+    <span class="text-muted-foreground/50 mr-2">*</span>
+    {text}
+  </p>
+{/snippet}
+
+{#snippet yamlKeyVal({ key, val, valUrl }: { key: string; val: string; valUrl?: string })}
+  <p>
+    <span class="text-muted-foreground">{key}:</span>
+    {#if valUrl != null}
+      <Button variant="link" href={valUrl} target="_blank" class="h-fit p-0 text-lg font-normal">
+        <p class="flex">
+          <span class="text-muted-foreground/50">[</span>
+          {val}
+          <span class="text-muted-foreground/50">](gen.tech)</span>
+        </p>
+      </Button>
+    {:else}
+      {val}
+    {/if}
+  </p>
+{/snippet}
+
+<p class="mb-2">Hi! My name is Mark.</p>
+
+{@render line({ text: 'I am interested in economics, machine learning, design, and, above all, great software.' })}
+{@render line({ text: 'I enjoy building pretty apps using Svelte and sometimes Flutter.' })}
+
+<!-- {@render separator()} -->
+
+{@render heading({ text: 'Quick info' })}
+
+<div class="font-mono">
+  <p class="text-muted-foreground/50">```yaml</p>
+  {@render yamlKeyVal({ key: 'Age', val: '22' })}
+  {@render yamlKeyVal({ key: 'Location', val: 'Kyiv, Ukraine' })}
+  {@render yamlKeyVal({ key: 'Languages', val: 'English, Ukrainian' })}
+
+  {@render yamlKeyVal({ key: 'Occupation', val: 'Genesis Tech', valUrl: 'https://gen.tech' })}
+
+  <p class="text-muted-foreground/50">```</p>
+</div>
+
+{@render heading({ text: 'Links' })}
+
+{#snippet ref({ url, text, isCopy }: { url: string; text: string; isCopy?: boolean })}
+  <div>
+    <Button
+      variant={isCopy ? 'ghost' : 'link'}
+      href={isCopy ? undefined : 'https://' + url}
+      target={isCopy ? undefined : '_blank'}
+      onclick={() => {
+        console.log('a123');
+      }}
+      class="h-fit !p-0 text-lg font-normal">
+      <p class="flex font-mono">
+        <span class="text-muted-foreground/50 mr-2">[1]:</span>
+        <span class="text-muted-foreground/50">&lt;</span>
+        <span class="text-foreground/70">{url}</span>
+        <span class="text-muted-foreground/50 mr-2">&gt;</span>
+
+        <span class="text-muted-foreground/50">"</span>
+        <span>{text}</span>
+        <span class="text-muted-foreground/50">"</span>
+      </p>
+      {#if isCopy}
+        <Copy class="text-muted-foreground/60 ml-2 size-4" />
+      {:else}
+        <ExternalLink class="text-muted-foreground/60 ml-2 size-4" />
+      {/if}
+    </Button>
+  </div>
+{/snippet}
+
+{@render ref({ url: 'mark.motliuk@gmail.com', text: 'Email', isCopy: true })}
+{@render ref({ url: 't.me/mmar4ik', text: 'Telegram' })}
+{@render ref({ url: 'github.com/marchellodev', text: 'GitHub' })}
+{@render ref({ url: 'linkedin.com/in/mark-motliuk', text: 'LinkedIn' })}
+
+<!-- todo: -->
+<!-- - flickering of: -->
+<!-- ```Occupation:  -->
+<!-- [ -->
+<!-- Genesis Tech -->
+<!-- ](gen.tech) -->
+<!-- ``` -->
+<!---->
+<!-- - Links -->
+<!-- - Links space in the underscore on hover -->
+<!-- - Add a cat -->
+<!-- - monospace? -->
