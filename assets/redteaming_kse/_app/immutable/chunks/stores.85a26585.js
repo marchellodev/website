9d1d2a9e31eab4733c15220490349d9f1b5a58d2
marchellodev <marchellodev@gmail.com>
2025-11-09 23:00:08 +0100

    add redteaming kse

---
+++ assets/redteaming_kse/_app/immutable/chunks/stores.85a26585.js
@@ -0,0 +1,1 @@
+import"./index.95dbb1e2.js";import{d as r}from"./singletons.8a54fcb6.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},o={subscribe(s){return e().navigating.subscribe(s)}};export{o as n,a as p};
