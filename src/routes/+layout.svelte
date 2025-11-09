2e1b3682dd0bf96e1d5c42c30e0c2e1563a3a059
marchellodev <marchellodev@gmail.com>
2025-11-09 22:09:44 +0100

    add theme switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,21 @@
   import { page } from '$app/state';
   import { Toaster } from '$lib/shadcn/components/ui/sonner';
 
+  import { ModeWatcher } from 'mode-watcher';
+
+  import SunIcon from '@lucide/svelte/icons/sun';
+  import MoonIcon from '@lucide/svelte/icons/moon';
+
+  import { toggleMode } from 'mode-watcher';
+
   let { children } = $props();
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
+
+<ModeWatcher />
 
 {#snippet navButton(opts: { text: string; url: string; newTab?: boolean })}
   {@const isActive = page.url.pathname == opts.url}
@@ -32,9 +41,17 @@
 <Toaster />
 
 <div class="mx-auto max-w-5xl p-2 font-mono text-lg">
-  <header class="bg-muted px-6 py-4">
-    {@render navButton({ text: 'About me', url: '/' })}
-    {@render navButton({ text: 'CV', url: 'https://mark.vin/cv.pdf', newTab: true })}
+  <header class="bg-muted flex justify-between px-6 py-4">
+    <div class="flex">
+      {@render navButton({ text: 'About me', url: '/' })}
+      {@render navButton({ text: 'CV', url: 'https://mark.vin/cv.pdf', newTab: true })}
+    </div>
+
+    <Button onclick={toggleMode} variant="outline" size="icon">
+      <SunIcon class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90" />
+      <MoonIcon class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0" />
+      <span class="sr-only">Toggle theme</span>
+    </Button>
   </header>
   <main class="bg-muted/30 px-6 py-6">
     {@render children()}
